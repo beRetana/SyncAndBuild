513e75bf4b66ce483a090c4869d121f61718e898
Brandon Eduardo Retana García <142031627+beRetana@users.noreply.github.com>
2026-02-14 18:00:37 -0800

    Created structured flow of logic for each step of installation. UI and user input intake are missing.

--- SynchAndBuildInstaller.py
+++ SynchAndBuildInstaller.py
@@ -6,6 +6,8 @@ import xml.etree.ElementTree as ET
 from pathlib import Path
 import tkinter as tk
 from tkinter import ttk, messagebox
+from enum import Enum
+from xml.dom import DomstringSizeErr
 
 # The tool cannot run without these files
 REQUIRED_SOURCE_FILES = [
@@ -200,20 +202,22 @@ def check_source_files_exist(app_path=None)-> bool:
             return False
 
     return True
-           
-# this is for now, we will add GUI later    
-def check_p4_config():
-    """Check if the p4 config file exists"""
-    config_path = get_p4_config_path()
+
+def check_p4_connection()-> bool:
+    """Check if the p4 CLI is connected to the server"""
     
-    # Check variables exist
-    # if they do create a new file
-    # if not ask user for input
+    env = os.environ.copy()
+    result = subprocess.run(
+        ["p4", "info"],
+        env=env,
+        capture_output=True,
+        text=True,
+        timeout=P4_TIME_OUT)
     
-    if (not config_path) or (not config_path.exists()):
-        create_p4_config()
-    else:
-        set_config(config_path)
+    if result.returncode != 0:
+        return False
+    
+    return True
 
 def is_custom_tool_defined(custom_tool_file: Path, custom_tool_name: str)-> bool:
     """Check if a custom tool is defined in p4v"""
@@ -282,16 +286,138 @@ def fix_existing_custom_tool(custom_tool_file: Path, custom_tool_name: str, bat_
     tree.write(custom_tool_file, encoding="UTF-8", xml_declaration=True)
     
     return define_custom_tool(custom_tool_file, custom_tool_name, bat_path, starting_folder)
-        
+
 class InstallerGUI:
     def __init__(self):
         pass
-        
-        
+            
+class LogType(Enum):
+    INFO = 0
+    WARNING = 1
+    SUCCESS = 2
+    ERROR = 3
+
 class ToolInstaller:
     
     def __init__(self):
+        self._app_path = get_app_path()
+        self._project_path = get_project_path(self._app_path)
+        self._uproject_path = get_uproject_path(self._project_path)
+        self._log_file = self._app_path.joinpath("Logs", "installer.log")
+        self._buffered_log = ""
+        
+    def _check_project_structure(self)-> bool:
+        """Check if the project structure is correct"""
+        self._log(LogType.INFO, "Checking for correct tool structure...")
+        
+        if not check_source_files_exist(self._app_path):
+            self._log(LogType.ERROR, "Source files are missing, please check the tool structure.")
+            return False
+        self._log(LogType.SUCCESS, "Source files found.")
+
+        self._log(LogType.INFO, "Checking for Project directory...")
+        if self._project_path is None:
+            self._log(LogType.ERROR, "Project directory not found.")
+            return False
+        self._log(LogType.SUCCESS, "Project directory found at: " + str(self._project_path))
+
+        self._log(LogType.INFO, "Checking for .uproject file...")
+        if self._uproject_path is None:
+            self._log(LogType.ERROR, ".uproject file not found.")
+            return False
+        self._log(LogType.SUCCESS, ".uproject file found at: " + str(self._uproject_path))
+        
+        return True
+    
+    def _check_p4(self)-> bool:
+
+        self._log(LogType.INFO, "Checking for correct P4, and P4V installation...")
+        
+        self._log(LogType.INFO, "Checking for p4 CLI...")
+        p4_path = get_p4_path()
+        if p4_path is None:
+            self._log(LogType.ERROR, "p4 CLI not found.")
+            return False
+        self._log(LogType.SUCCESS, "p4 CLI found at: " + str(p4_path))
+
+        self._log(LogType.INFO, "Checking for p4 CLI connection...")
+        if check_p4_connection():
+            self._log(LogType.ERROR, "p4 CLI is not connected to the server.")
+            return False
+        self._log(LogType.SUCCESS, "p4 CLI is connected to the server.")
+
+        self._log(LogType.INFO, "Checking for p4v...")
+        p4v_path = get_p4v_path()
+        if p4v_path is None:
+            self._log(LogType.ERROR, "p4v not found.")
+            return False
+        self._log(LogType.SUCCESS, "p4v found at: " + str(p4v_path))
+
+        return True
+        
+    def _setup_p4_config_(self)-> bool:
+        """Check if the p4 config file exists"""
+        return False
+    
+    def _setup_custom_tool_(self)-> bool:
+        """Check if the custom tool is defined in p4v"""
+        return False
+        
+    def _clean_log_file(self):
+        with open(self._log_file, "w") as log_file:
+            log_file.write("")
+            
+    def _flush_to_log_file(self):
+        if self._buffered_log == "":
+            return
+        
+        with open(self._log_file, "a") as log_file:
+            log_file.write(self._buffered_log)
+            self._buffered_log = ""
+    
+    def _log(self, log_type: LogType, message: str):
+        
+        # logic for showing the log in the GUI
+        
+        self._buffered_log += f"{log_type.name}: {message}\n"
+        
+    def _abort_installation(self):
+        self._flush_to_log_file()
         pass
+    
+    def _installation_finished(self):
+        self._log(LogType.SUCCESS, "Installation finished successfully.")
+        self._flush_to_log_file()
+        pass
+
+    def install(self):
+        """Install the tool"""
+
+        self._clean_log_file()
+        self._log(LogType.INFO, "Starting installation...")
+
+        installation_steps = [
+            self._check_project_structure,
+            self._check_p4,
+            self._setup_p4_config_,
+            self._setup_custom_tool_]
+
+        for step in installation_steps:
+            self._flush_to_log_file()
+            if not step():
+                self._abort_installation()
+                return
+
+        self._installation_finished()
+    
+    def run(self):
+        
+        try:
+            self.install()
+        except Exception as e:
+            self._log(LogType.ERROR, str(e))
+        finally:
+            self._flush_to_log_file()
 
 if __name__ == "__main__":
     pass
